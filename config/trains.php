8bd16442a6aab7f2f4569736cbf31c318f9cc21f
AdrianaSantorio <adriana.santorio@gmail.com>
2022-03-25 15:52:33 +0100

    completed phase 2

---
+++ config/trains.php
@@ -0,0 +1,48 @@
+<?php
+$trains = [
+    [
+        'company' => 'Trenitalia',
+        'departure_station' => 'Firenze',
+        'arrival_station' => 'Roma',
+        'departure_time' => '16:50:00',
+        'arrival_time' => '18:10:00',
+        'train_code' => '2217',
+        'number_of_carriages' => 8,
+        'on_time' => 0,
+        'cancelled' => 0,
+    ],
+    [
+        'company' => 'ITALO',
+        'departure_station' => 'Roma',
+        'arrival_station' => 'Napoli',
+        'departure_time' => '07:10:00',
+        'arrival_time' => '08:50:00',
+        'train_code' => '9910',
+        'number_of_carriages' => 11,
+        'on_time' => 0,
+        'cancelled' => 1,
+    ],
+    [
+        'company' => 'SVG',
+        'departure_station' => 'Milano',
+        'arrival_station' => 'Parigi',
+        'departure_time' => '12:55:00',
+        'arrival_time' => '20:30:00',
+        'train_code' => '98473',
+        'number_of_carriages' => 12,
+        'on_time' => 1,
+        'cancelled' => 0,
+    ],
+    [
+        'company' => 'TRENITALIA',
+        'departure_station' => 'Roma',
+        'arrival_station' => 'Bologna',
+        'departure_time' => '11:00:00',
+        'arrival_time' => '13:50:00',
+        'train_code' => '7932',
+        'number_of_carriages' => 10,
+        'on_time' => 1,
+        'cancelled' => 0,
+    ],
+
+];
