052fde2f9c66b41c5b67a04466a7a3fcf89d1270
AdrianaSantorio <adriana.santorio@gmail.com>
2022-03-25 15:20:17 +0100

    completed phase one

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -1,12 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
-<ul>
-    @foreach ($trains as $train)
-        <li>{{$train->train_code}}</li>
-    @endforeach
-</ul>
+<div class="container my-5">
+    <div class="row g-4">
+        @foreach ($trains as $train)
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"> Train {{$train->company}} - {{$train->train_code}}</h5>
+                  <p class="card-text">From <strong>{{$train->departure_station}}</strong> to <strong>{{$train->arrival_station}}</strong></p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Departure time {{$train->departure_time}}</li>
+                  <li class="list-group-item">Arrival Time {{$train->arrival_time}}</li>
+                  @if ($train->cancelled == 1)
+                    <li class="list-group-item text-end"><em>Cancelled</em></li>
+                  @elseif ($train->on_time == 0)
+                  <li class="list-group-item text-end"><em>Delayed</em></li>
+                  @else
+                  <li class="list-group-item text-end"><em>On time</em></li>
+                  @endif
+                </ul>
+            </div>
+        </div>
+        @endforeach
+    </div>
 
-
-
+</div>
 @endsection
